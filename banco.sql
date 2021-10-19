26b9469396c0067552695643c75cd89de91b11bd
Ana Clara <acsn4@aluno.ifal.edu.br>
2021-10-19 20:14:03 -0300

    iniciando_ajax

---
+++ banco.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE pweb_ajax;
+USE pweb_ajax;
+
+CREATE TABLE  `pweb_ajax`.`usuarios` (
+  `id_usuario` int(10) unsigned NOT NULL auto_increment,
+  `nome` varchar(200) NOT NULL default '',
+  `email` varchar(200) NOT NULL default '',
+  `senha` varchar(45) NOT NULL default '',
+  `cpf` varchar(14) NOT NULL default '',
+  PRIMARY KEY  (`id_usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
